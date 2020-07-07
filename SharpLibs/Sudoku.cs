bfd8683bf67dc45df81557e719531425305db05a
Matt Waldron <matt.waldron@livanova.com>
2020-07-07 08:23:55 -0500

    two working sudoku algorithms that solve the easy puzzle

--- SharpLibs/Sudoku.cs
+++ SharpLibs/Sudoku.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text;
 
 namespace Sudoku
 {
@@ -9,23 +10,21 @@ namespace Sudoku
     {
         public int Row { get; set; }
         public int Column { get; set; }
-        public int Block => ((Row % 3) + ((Column % 3) * 3));
-        public int? Value { get; private set; }
-        public bool HasValue => Value.HasValue;
-
+        public int Block => ((Row / 3) + ((Column / 3) * 3));
+        private int? _value;
+        public int Value => _value.Value;
+        public bool HasValue => _value.HasValue;
         public void SetValue(int value, IEnumerable<SudokuSquare> board = null)
         {
-            Value = value;
-            Exclusions = SudokuIterators.OneToNine.Where(v => v != Value).ToList();
-            if (board != null)
+            _value = value;
+            Exclusions = SudokuIterators.OneToNine.Where(v => v != _value).ToList();
+        }
+
+        public void Exclude(int value)
+        {
+            if (!Exclusions.Contains(value))
             {
-                foreach (var s in board.AllRelated(this))
-                {
-                    if (!s.HasValue)
-                    {
-                        s.Exclusions.Add(value);
-                    }
-                }
+                Exclusions.Add(value);
             }
         }
 
@@ -35,47 +34,209 @@ namespace Sudoku
                Row == s.Row
             || Column == s.Column
             || Block == s.Block;
+    }
 
+    public class SudokuBoard
+    {
+        SudokuSquare[,] board;
+
+        public SudokuBoard()
+        {
+            board = new SudokuSquare[9, 9];
+
+            foreach (var r in SudokuIterators.ZeroToEight)
+            {
+                foreach (var c in SudokuIterators.ZeroToEight)
+                {
+                    board[r, c] = new SudokuSquare { Row = r, Column = c };
+                }
+            }
+        }
+
+        public SudokuSquare this[int r, int c] => board[r, c];
+
+        public void SetValue(SudokuSquare s, int value)
+        {
+            s.SetValue(value);
+            foreach (var ss in AllRelated(s))
+            {
+                ss.Exclude(value);
+            }
+        }
+
+        public IEnumerable<SudokuSquare> Squares
+        {
+            get
+            {
+                foreach (var r in SudokuIterators.ZeroToEight)
+                {
+                    foreach (var c in SudokuIterators.ZeroToEight)
+                    {
+                        yield return board[r, c];
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("-------------");
+            foreach (var enumerator in new[] {Enumerable.Range(0, 3), Enumerable.Range(3, 3), Enumerable.Range(6, 3)})
+            {
+                foreach (var r in enumerator)
+                {
+                    sb.Append("|");
+                    foreach (var enumerator2 in new[] { Enumerable.Range(0, 3), Enumerable.Range(3, 3), Enumerable.Range(6, 3) })
+                    {
+                        foreach (var c in enumerator2)
+                        {
+                            sb.Append(board[r, c].HasValue ? board[r, c].Value.ToString() : " ");
+                        }
+                        sb.Append("|");
+                    }
+
+                    sb.AppendLine("");
+                }
+                sb.AppendLine("-------------");
+            }
+
+            return sb.ToString();
+        }
+
+        public IEnumerable<SudokuSquare> GetRow(int r)
+        {
+            foreach (var c in SudokuIterators.ZeroToEight)
+            {
+                yield return board[r, c];
+            }
+        }
+
+        public IEnumerable<SudokuSquare> GetColumn(int c)
+        {
+            foreach (var r in SudokuIterators.ZeroToEight)
+            {
+                yield return board[r, c];
+            }
+        }
+
+        public IEnumerable<SudokuSquare> GetBlock(int b)
+        {
+            foreach (var r in SudokuIterators.ZeroToEight)
+            {
+                foreach (var c in SudokuIterators.ZeroToEight)
+                {
+                    var s = board[r, c];
+                    if (s.Block == b)
+                    {
+                        yield return s;
+                    }
+                }
+            }
+        }
+        
+
+        public int NumFound => Squares.Count(s => s.HasValue);
+        public int NumEmpty => Squares.Count(s => !s.HasValue);
+
+        public bool IsValidComplete
+        {
+            get
+            {
+                if (NumEmpty != 0)
+                {
+                    return false;
+                }
+                foreach (var enumerator in new[] { EnumerateRows(), EnumerateColumns(), EnumerateBlocks() })
+                {
+                    foreach (var set in enumerator)
+                    {
+                        if (set.Select(v => v.Value).Distinct().Count() != 9)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+        }
+
+
+        public IEnumerable<SudokuSquare> AllRelated(SudokuSquare square) =>
+            Squares.Where(square.IsRelatedTo);
+        public IEnumerable<IEnumerable<SudokuSquare>> EnumerateRows() =>
+            SudokuIterators.ZeroToEight.Select(GetRow);
+        public IEnumerable<IEnumerable<SudokuSquare>> EnumerateColumns() =>
+            SudokuIterators.ZeroToEight.Select(GetColumn);
+        public IEnumerable<IEnumerable<SudokuSquare>> EnumerateBlocks() =>
+            SudokuIterators.ZeroToEight.Select(GetBlock);
     }
 
     public static class SudokuIterators
     {
         public static IEnumerable<int> ZeroToEight => Enumerable.Range(0, 9);
         public static IEnumerable<int> OneToNine => Enumerable.Range(1, 9);
-        public static IEnumerable<SudokuSquare> GetRow(this IEnumerable<SudokuSquare> board, int r) => board.Where(s => s.Row == r);
-        public static IEnumerable<SudokuSquare> GetColumn(this IEnumerable<SudokuSquare> board, int c) => board.Where(s => s.Column == c);
-        public static IEnumerable<SudokuSquare> GetBlock(this IEnumerable<SudokuSquare> board, int b) => board.Where(s => s.Block == b);
-
-        
-        public static IEnumerable<(int, IEnumerable<SudokuSquare>)> EnumerateRows(this IEnumerable<SudokuSquare> board) =>
-            ZeroToEight.Select(i => (i, board.GetRow(i)));
-        public static IEnumerable<(int, IEnumerable<SudokuSquare>)> EnumerateColumns(this IEnumerable<SudokuSquare> board) =>
-            ZeroToEight.Select(i => (i, board.GetColumn(i)));
-        public static IEnumerable<(int, IEnumerable<SudokuSquare>)> EnumerateBlocks(this IEnumerable<SudokuSquare> board) =>
-            ZeroToEight.Select(i => (i, board.GetBlock(i)));
-        public static IEnumerable<int> FoundValues(this IEnumerable<SudokuSquare> board) =>
-            board.Where(s => s.HasValue).Select(s => s.Value.Value);
-        public static IEnumerable<SudokuSquare> CreateBoard() =>
-            ZeroToEight.SelectMany(r => ZeroToEight, (r, c) => new SudokuSquare{Row = r, Column = c}).ToList();
-
-        public static IEnumerable<SudokuSquare> AllRelated(this IEnumerable<SudokuSquare> board, SudokuSquare square) =>
-            board.Where(square.IsRelatedTo);
+       
     }
 
     public static class SudokuAlgorithms
     {
-        public static bool TrySquareExclusions(this SudokuSquare s, out int value)
+        public static bool TrySquareExclusions(this SudokuBoard b, out SudokuSquare newlySolved)
         {
-            value = 0;
-            if (s.HasValue)
+            newlySolved = null;
+            foreach (var s in b.Squares)
             {
-                return false;
+                if (s.HasValue)
+                {
+                    continue;
+                }
+
+                if (s.Exclusions.Count == 8)
+                {
+                    var value = SudokuIterators.OneToNine.Except(s.Exclusions).Single();
+                    b.SetValue(s, value);
+                    newlySolved = s;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static SudokuSquare OnlyWithoutExclusion(IEnumerable<SudokuSquare> squares, SudokuBoard board)
+        {
+            var squareList = squares.ToList();
+            var emptyList = squares.Where(s => !s.HasValue).ToList();
+            var remaining =
+                SudokuIterators.OneToNine.Except(squareList.Where(s => s.HasValue).Select(s => s.Value));
+            foreach (var r in remaining)
+            {
+                if (emptyList.Count(e => e.Exclusions.Contains(r)) == emptyList.Count - 1)
+                {
+                    var sq = emptyList.Single(e => !e.Exclusions.Contains(r));
+                    board.SetValue(sq, r);
+                    return sq;
+                }
             }
 
-            if (s.Exclusions.Count == 8)
+            return null;
+        }
+
+        public static bool TryOnlyWithoutExclusion(this SudokuBoard b, out SudokuSquare newlyFound)
+        {
+            newlyFound = null;
+
+            foreach (var enumerator in new[] {b.EnumerateRows(), b.EnumerateColumns(), b.EnumerateBlocks()})
             {
-                value = SudokuIterators.OneToNine.Except(s.Exclusions).Single();
-                return true;
+                foreach (var set in enumerator)
+                {
+                    var sq = OnlyWithoutExclusion(set, b);
+                    if (sq != null)
+                    {
+                        newlyFound = sq;
+                        return true;
+                    }
+                }
             }
 
             return false;
--- SudokuTest/SudokuTests.cs
+++ SudokuTest/SudokuTests.cs
@@ -8,9 +8,9 @@ namespace SudokuTest
 {
     public class SudokuTestHelp
     {
-        public static IEnumerable<SudokuSquare> LoadBoardFromText(string text)
+        public static SudokuBoard LoadBoardFromText(string text)
         {
-            var board = SudokuIterators.CreateBoard();
+            var board = new SudokuBoard();
             int row = 0;
             foreach (var line in text.Split('\n'))
             {
@@ -19,7 +19,7 @@ namespace SudokuTest
                 {
                     if (c != ' ')
                     {
-                        board.Single(s => s.Row == row && s.Column == col).SetValue(int.Parse(c.ToString()), board);
+                        board.SetValue(board[row,col], int.Parse(c.ToString()));
                     }
 
                     col++;
@@ -31,60 +31,65 @@ namespace SudokuTest
             return board;
         }
 
-        public static string PrintBoard(IEnumerable<SudokuSquare> board)
-        {
-            var text = "";
-            foreach (var r in SudokuIterators.ZeroToEight)
-            {
-                foreach (var c in SudokuIterators.ZeroToEight)
-                { 
-                    var square = board.Single(s => s.Row == r && s.Column == c);
-                    text += square.HasValue ? square.Value.ToString() : " ";
-                }
-
-                text += "\n";
-            }
-
-            return text;
-        }
+        
     }
 
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void SquareExclusions_EasyPuzzle()
         {
+            var board = SudokuTestHelp.LoadBoardFromText(
+@" 98  54  
+  3697  8
+7 1  4   
+  91    2
+ 72   13 
+4    25  
+   2  3 5
+9  4568  
+  59  74");
+            while (true)
+            {
+                Debug.Write(board.ToString());
+                Debug.WriteLine("");
+                SudokuSquare s;
+                if (board.TrySquareExclusions(out s))
+                {
+                    continue;
+                }
+                break;
+            }
+            Assert.IsTrue(board.IsValidComplete);
         }
 
         [Test]
-        public void EasyPuzzle()
+        public void OnlyWithoutExclusion_EasyPuzzle()
         {
             var board = SudokuTestHelp.LoadBoardFromText(
-@"     41  
-71 8  3 5
-957 83   
-1   4   2
-   16 758
- 91  5  3
-8 2  6 14
-  57     ");
-            var iterate = true;
-            while (iterate)
+@" 98  54  
+  3697  8
+7 1  4   
+  91    2
+ 72   13 
+4    25  
+   2  3 5
+9  4568  
+  59  74");
+            while (true)
             {
-                Debug.Write(SudokuTestHelp.PrintBoard(board));
-                Debug.WriteLine("----------");
-                iterate = false;
-                foreach (var s in board)
+                Debug.Write(board.ToString());
+                Debug.WriteLine("");
+                SudokuSquare s;
+                if (board.TryOnlyWithoutExclusion(out s))
                 {
-                    if (s.TrySquareExclusions(out var v))
-                    {
-                        s.SetValue(v, board);
-                        iterate = true;
-                    }
-
-                    break;
+                    continue;
                 }
+
+                break;
             }
+            Assert.IsTrue(board.IsValidComplete);
         }
     }
 }
